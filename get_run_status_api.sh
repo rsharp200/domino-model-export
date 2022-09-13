4de11ffd97672b237bbe8225f1ef762434e3586b
Ross Sharp <ross.sharp@dominodatalab.com>
2022-09-13 16:17:47 -0700

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/ross_sharp/ModelExportPipeline?executionId=631fb1efde659c33feb8eece

---
+++ get_run_status_api.sh
@@ -0,0 +1,3 @@
+curl --include \
+     --header "X-Domino-Api-Key: "3c3d51a08645aa66bf3bdef9ab0cc047fbdebd024124118d738458aebc483e64" \
+  'https://demo2.dominodatalab.com/v1/projects/${DOMINO_PROJECT_OWNER}/${DOMINO_PROJECT_NAME}/runs/6320efd54335d417d82b3309'
