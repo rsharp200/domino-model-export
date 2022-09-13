2367844b9be40829c86532d5de47f39af8f5a2c6
Ross Sharp <ross.sharp@dominodatalab.com>
2022-09-13 13:04:28 -0700

    Pushed from Domino: https://demo2.dominodatalab.com/workspace/ross_sharp/ModelExportPipeline?executionId=631fb1efde659c33feb8eece

--- test_model.sh
+++ test_model.sh
@@ -3,9 +3,9 @@
 set -e
 
 MODEL_ID="6320c021de659c33feb8efe5"
-MODEL_API_TOKEN="P4q84ATwNialWVxHeXRwO42VfOzQSx0OQFXLBMYKrqnUgcitaW8EZsS27LanNZgj"
+MODEL_API_TOKEN="P4q84ATwNialWVxHeXRwO42VfOzQSx0OQFXLBMYKrqnUgcitaW8EZsS27LanNZgj:P4q84ATwNialWVxHeXRwO42VfOzQSx0OQFXLBMYKrqnUgcitaW8EZsS27LanNZgj"
 
-TEST=$(curl ${DOMINO_API_HOST}/models/${MODEL_ID}/latest/model -s -H 'Content-Type: application/json' -d '{ "data": { "dropperc": 1000, "mins": 941, "consecmonths": 29, "income": 35000 } }' | grep "request_id" | wc -l)
+TEST=$(curl ${DOMINO_API_HOST}:443/models/${MODEL_ID}/latest/model -s -H 'Content-Type: application/json' -d '{ "data": { "dropperc": 1000, "mins": 941, "consecmonths": 29, "income": 35000 } }' -u $MODEL_API_TOKEN | grep "request_id" | wc -l)
 
 if [[ $TEST -gt 0 ]]; then
     echo "TEST: PASS"
